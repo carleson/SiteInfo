bd49fe6c39e8b68431f3f42768b5011b8d7e95cc
Johan Carleson <johan.carleson@gmail.com>
2013-06-28 12:02:55 +0200

    Moved some of the string methods into a external lib - TextLib.

--- SiteInfo/GUI/MainForm.cs
+++ SiteInfo/GUI/MainForm.cs
@@ -118,6 +118,8 @@ namespace SiteInfo
 				     
 				using (WebClient client = new WebClient ()) 
 				{
+					client.Headers.Add ("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+					
 				    _htmlOutput = client.DownloadString(txtURL.Text);
 				}
 				site = new SiteInfo(txtURL.Text,_htmlOutput, config);
--- SiteInfo/Source/SiteStatistics.cs
+++ SiteInfo/Source/SiteStatistics.cs
@@ -9,6 +9,8 @@ using System.Text;
 using System.Linq;
 using System.Windows.Forms;
 using System.Collections.Generic;
+using TextLib.Words;
+
 namespace SiteInfo
 {
 	/// <summary>
@@ -17,12 +19,14 @@ namespace SiteInfo
 	public class SiteStatistics
 	{
 		private SiteInfo _site;
+		private TextLib.Words.Word _words;
 			
 		public string name {get;set;}
 		
 		public SiteStatistics(SiteInfo site)
 		{
 			_site = site;
+			_words = new TextLib.Words.Word(site.Source);
 		}		
 		
 #region Properties
@@ -42,16 +46,16 @@ namespace SiteInfo
 			
 			StringBuilder sb = new StringBuilder();
 			
-			sb.Append(string.Format("Lenght data:{0}",statLengh));
+			sb.Append(string.Format("Lenght data:{0}", _words.Count()));
 			sb.AppendLine();
 			sb.AppendLine(string.Format("Size data:{0} bytes",GetSizeInBytes(_site.Source)));
-			sb.AppendLine(string.Format("Words:{0}",WordCount()));
-			sb.AppendLine(string.Format("Common words{0}",CommonWords()));
+			sb.AppendLine(string.Format("Words:{0}",_words.Count()));
+			sb.AppendLine(string.Format("Common words{0}",_words.CommonWords()));
 			//Word count section
-			sb.AppendLine(string.Format("links:{0}",WordCount("<a href=")));
-			sb.AppendLine(string.Format(".png-files:{0}",WordCount(".png")));
-			sb.AppendLine(string.Format(".jpg-files:{0}",WordCount(".jpg")));
-			sb.AppendLine(string.Format("Comments:{0}",WordCount("<!--")));
+			sb.AppendLine(string.Format("links:{0}",_words.Count("<a href=")));
+			sb.AppendLine(string.Format(".png-files:{0}",_words.Count(".png")));
+			sb.AppendLine(string.Format(".jpg-files:{0}",_words.Count(".jpg")));
+			sb.AppendLine(string.Format("Comments:{0}",_words.Count("<!--")));
 
 			
 			return sb.ToString();
@@ -63,28 +67,20 @@ namespace SiteInfo
 		/// <param name="word"></param>
 		/// <param name="text"></param>
 		/// <returns></returns>
-		private int WordCount(string word)
-		{
-			string text = _site.Source;
-			int pos = 0;
-			int count = 0;
-			pos = text.IndexOf(word);
-			while (pos != -1 && text.Length >= pos)
-			{
-				count++;
-				pos = _site.Source.IndexOf(word,pos+1);
-			}
-			return count;
-		}
+//		private int WordCount(string word)
+//		{
+//			string text = _site.Source;
+//			int pos = 0;
+//			int count = 0;
+//			pos = text.IndexOf(word);
+//			while (pos != -1 && text.Length >= pos)
+//			{
+//				count++;
+//				pos = _site.Source.IndexOf(word,pos+1);
+//			}
+//			return count;
+//		}
 		
-
-		private int WordCount()
-		{
-			string text = _site.Source;
-			
-			string[] words = text.Split(new string[] {" ", "\n", "\r"}, StringSplitOptions.RemoveEmptyEntries);
-            return words.Length;
-		}
 		
 		/// <summary>
 		/// Calculates the true size of a string, in bytes
@@ -100,61 +96,6 @@ namespace SiteInfo
 			return size;
 		}
 		
-		private string CommonWords()
-		{
-			string text = _site.Source;
-			
-			  // Number of words
-            string[] words = text.Split(new string[] {" ", "\n", "\r"}, StringSplitOptions.RemoveEmptyEntries);
-            //int numWords = words.Length;
-
-
-            // Number of letters
-//            int numLetters = 0;
-//            foreach (char c in text)
-//            {
-//                if (char.IsLetter(c))
-//                    numLetters ++;
-//            }
-
-
-            // Number of symbols
-//            int numSymbols = 0;
-//            foreach (char c in text)
-//            {
-//                if (char.IsPunctuation(c))
-//                    numSymbols ++;
-//            }
-
-
-            // Most common words
-            var dictionary = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
-
-            foreach (string word in words)
-            {
-                if (dictionary.ContainsKey(word))
-                    dictionary[word] = dictionary[word] + 1;
-                else
-                    dictionary[word] = 1;
-            }
-
-            var sortedDictionary = from item in dictionary
-                              orderby item.Value descending
-                              select item;
-
-            string[] commonWords = new string[3];
-            int count = 0;
-
-            foreach (KeyValuePair<string, int> item in sortedDictionary.Take(3))
-            {
-                commonWords[count] = item.Key;
-                count++;
-            }
-
-			 Console.WriteLine("Top three most common words: {0}, {1}, {2}", commonWords[0], commonWords[1], commonWords[2]);
-			 string output = string.Format("Top three most common words: {0}, {1}, {2}", commonWords[0], commonWords[1], commonWords[2]);
-			 return output;
-		}
 		
 #endregion
 
